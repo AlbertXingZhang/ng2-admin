d90be193587c658bf36d8e84d4ee49ceab2f1da5
Xing <zhangxingW@hotmail.com>
2016-06-21 15:22:58 -0700

    Add task for coping dist to wwwroot.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task("default", ["webpack-dev-server"]);
 // Disadvantage: Requests are not blocked until bundle is available,
 //               can serve an old app on refresh
 gulp.task("build-dev", ["webpack:build-dev"], function () {
-    gulp.watch(["app/**/*"], ["webpack:build-dev"]);
+    gulp.watch(["src/**/*"], ["webpack:build-dev"]);
 });
 
 // Production build
@@ -77,4 +77,15 @@ gulp.task("webpack-dev-server", function (callback) {
         if (err) throw new gutil.PluginError("webpack-dev-server", err);
         gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
     });
+});
+
+var rimraf = require("rimraf");
+
+gulp.task("clean", function (cb) {
+    rimraf("{wwwroot,dist}/**/*", cb);
+});
+
+gulp.task("dist", function () {
+    return gulp.src("dist/**/*")
+        .pipe(gulp.dest("wwwroot"));
 });
